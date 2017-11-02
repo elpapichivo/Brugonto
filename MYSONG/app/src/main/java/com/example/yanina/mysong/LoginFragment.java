af85ccd202369d55bcdac1df86a22b809f2bf612
Unknown <gjtole@gmail.com>
2017-11-02 10:26:14 -0300

    Auto stash before merge of "master" and "origin/master"

--- MYSONG/app/src/main/java/com/example/yanina/mysong/LoginFragment.java
+++ MYSONG/app/src/main/java/com/example/yanina/mysong/LoginFragment.java
@@ -2,10 +2,14 @@ package com.example.yanina.mysong;
 
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 /**
@@ -13,6 +17,7 @@ import android.view.ViewGroup;
  */
 public class LoginFragment extends Fragment {
 
+    private View view;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -23,12 +28,43 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
 
+        view = inflater.inflate(R.layout.fragment_login, container, false);
 
+        Button botonIngresar = (Button) view.findViewById(R.id.ButtonIngresar);
+        botonIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Boton Ingresar (En construccion)", Toast.LENGTH_SHORT).show();
+            }
+        });
 
+        Button botonRegistrarse = (Button) view.findViewById(R.id.ButtonRegistrarse);
+        botonRegistrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Boton Registrarse (En construccion)", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
     }
 
+    public void contrasenia (View botonIngresar){
+
+        TextInputLayout editTextContrasenia;
+        editTextContrasenia = (TextInputLayout) view.findViewById(R.id.TextImputContrasenia);
+
+        EditText editText = (EditText) view.findViewById(R.id.EditTextContrasenia);
+
+        String contrasenia = editText.getText().toString();
+
+        if (contrasenia.length()<8){
+            editTextContrasenia.setError("La contraseña debe contener como minimo 8 caracteres");
+        }else {
+            editTextContrasenia.setError(null);
+        }
+    }
 
 
 }
