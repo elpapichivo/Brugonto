87aed8b325e313e8163bd20314d76826cb25e4b2
ff <ff>
2017-11-09 09:55:26 -0300

    algo detalle

--- MYSONG/app/src/main/java/com/example/yanina/mysong/Dao/DaoArtistas.java
+++ MYSONG/app/src/main/java/com/example/yanina/mysong/Dao/DaoArtistas.java
@@ -14,14 +14,14 @@ import java.util.List;
 public class DaoArtistas {
     public void obtenerArtista(ResultListener<List<Artista>> listResultListener){
         List<Artista> listaDeArtistas = new ArrayList<>();
-        listaDeArtistas.add(new Artista("Foo Fighters","no hay","rock", R.drawable.foo_fighters));
-        listaDeArtistas.add(new Artista("Guns n Rose ","no hay","rock",R.drawable.guns_n_roses_logo));
-        listaDeArtistas.add(new Artista("Linkin Park","no hay","rock",R.drawable.linkin_park));
-        listaDeArtistas.add(new Artista("Metallica","no hay","rock",R.drawable.metallica));
-        listaDeArtistas.add(new Artista("Nirvana","no hay","rock",R.drawable.nirvana));
-        listaDeArtistas.add(new Artista("Pink floyd","no hay","rock",R.drawable.pinkfloyd));
-        listaDeArtistas.add(new Artista("The Rolling Stone","no hay","rock",R.drawable.the_rolling_stones));
-        listaDeArtistas.add(new Artista("The Who","no hay","rock",R.drawable.the_who));
+        listaDeArtistas.add(new Artista("Foo Fighters","detalles del artista","rock", R.drawable.foo_fighters));
+        listaDeArtistas.add(new Artista("Guns n Rose ","detalles del artista","rock",R.drawable.guns_n_roses_logo));
+        listaDeArtistas.add(new Artista("Linkin Park","detalles del artista","rock",R.drawable.linkin_park));
+        listaDeArtistas.add(new Artista("Metallica","detalles del artista","rock",R.drawable.metallica));
+        listaDeArtistas.add(new Artista("Nirvana","detalles del artista","rock",R.drawable.nirvana));
+        listaDeArtistas.add(new Artista("Pink floyd","detalles del artista","rock",R.drawable.pinkfloyd));
+        listaDeArtistas.add(new Artista("The Rolling Stone","detalles del artista","rock",R.drawable.the_rolling_stones));
+        listaDeArtistas.add(new Artista("The Who","detalles del artista","rock",R.drawable.the_who));
         listResultListener.finish(listaDeArtistas);
 
     }
